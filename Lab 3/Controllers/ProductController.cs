c9d1e2bfb5d413ea50836c4b52b8e4ab64fcbd7c
Yến Nguyễn <yn040620052@gmail.com>
2025-05-19 21:45:52 +0700

    Lab3

---
+++ Lab 3/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab_3.Models;
+
+namespace Lab_3.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Details()
+        {
+            Product product = new Product
+            {
+                Id = 1,
+                Name = "Sample Product",
+                Price = 19.99m
+            };
+            return View(product);
+        }
+    }
+}
